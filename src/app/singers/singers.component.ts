eab1adabbaae451448aee49569e3fc339e0519b9
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 11:29:27 -0700

    delete singers array from singers component; keep declaration

--- src/app/singers/singers.component.ts
+++ src/app/singers/singers.component.ts
@@ -10,18 +10,9 @@ import { Singer } from '../singer.model';
   styleUrls: ['./singers.component.css']
 })
 export class SingersComponent implements OnInit {
+  singers: Singer[];
 
   constructor(private router: Router) { }
-
-  singers: Singer[] = [
-    new Singer("Lennie Cottrell", "Tenor 2", "conductor, co-director", 0, 1),
-    new Singer("Nicole Freed", "Soprano 1", "co-director", 1, 2),
-    new Singer("Nathan Daniel", "Tenor 1", "treasurer, section leader", 2, 3),
-    new Singer("Alex Ibiza", "Alto 1", "secretary, section leader", 3, 4),
-    new Singer("Omeran Abatu", "Baritone/Bass 1", "webmaster, section leader", 4, 5),
-    new Singer("Cindy Serrano", "Soprano 2", "singer, section leader", 5, 6),
-    new Singer("Kimiko Shuri", "Alto 2", "singer, publicity", 6, 7)
-  ];
 
   ngOnInit() {
   }
