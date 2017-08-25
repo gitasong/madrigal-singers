f94e74bdfd47a1167ddbbfefa8466a45e61958e4
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 11:26:31 -0700

    update singer data

--- src/app/singer-data.ts
+++ src/app/singer-data.ts
@@ -7,5 +7,5 @@ export const SINGERS: Singer[] = [
   new Singer("Alex Ibiza", "Alto 1", "secretary, section leader", 3, 4),
   new Singer("Omeran Abatu", "Baritone/Bass 1", "webmaster, section leader", 4, 5),
   new Singer("Cindy Serrano", "Soprano 2", "singer, section leader", 5, 6),
-  new Singer("Kimiko Shuri", "Soprano 1", "singer, publicity", 6, 7)
+  new Singer("Kimiko Shuri", "Alto 2", "singer, publicity", 6, 7)
 ];
--- src/app/singers/singers.component.ts
+++ src/app/singers/singers.component.ts
@@ -17,10 +17,10 @@ export class SingersComponent implements OnInit {
     new Singer("Lennie Cottrell", "Tenor 2", "conductor, co-director", 0, 1),
     new Singer("Nicole Freed", "Soprano 1", "co-director", 1, 2),
     new Singer("Nathan Daniel", "Tenor 1", "treasurer, section leader", 2, 3),
-    new Singer("Alex Ibiza", "Soprano 2", "secretary, section leader", 3, 4),
+    new Singer("Alex Ibiza", "Alto 1", "secretary, section leader", 3, 4),
     new Singer("Omeran Abatu", "Baritone/Bass 1", "webmaster, section leader", 4, 5),
-    new Singer("Cindy Serrano", "Soprano 2", "singer", 5, 6),
-    new Singer("Kimiko Shuri", "Soprano 1", "singer", 6, 7)
+    new Singer("Cindy Serrano", "Soprano 2", "singer, section leader", 5, 6),
+    new Singer("Kimiko Shuri", "Alto 2", "singer, publicity", 6, 7)
   ];
 
   ngOnInit() {
