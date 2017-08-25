f696946d8d9abf2f25872ffe87e1d85db205307b
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 13:42:03 -0700

    add SingerService to providers list

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -5,8 +5,10 @@ import { Singer } from '../singer.model';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [SingerService]
 })
+
 export class AdminComponent implements OnInit {
 
   constructor(private singerService: SingerService) { }
