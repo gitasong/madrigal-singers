171e27cbf49bdce3d2cd1aeff5fbc19f887fd09c
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 08:31:30 -0700

    create singers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SingersComponent } from './singers/singers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingersComponent
   ],
   imports: [
     BrowserModule,
