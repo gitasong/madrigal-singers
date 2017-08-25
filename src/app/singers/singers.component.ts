9444fdbc5db70efd3295d5045d832d13c7dc2a57
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 11:38:06 -0700

    create ngOnInit() method to retrieve singer service data on init

--- src/app/singers/singers.component.ts
+++ src/app/singers/singers.component.ts
@@ -17,6 +17,7 @@ export class SingersComponent implements OnInit {
   constructor(private router: Router, private singerService: SingerService) { }
 
   ngOnInit() {
+    this.singers = this.singerService.getSingers();
   }
 
   goToDetail(clickedSinger: Singer) {
