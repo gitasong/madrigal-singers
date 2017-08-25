9d546cd32eaec16a69320a02c8e19007cc0f4a4f
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 12:30:15 -0700

    import AngularFireModule, AngularFireDatabaseModule, and masterFirebaseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
 
 import { AppComponent } from './app.component';
 import { SingersComponent } from './singers/singers.component';
