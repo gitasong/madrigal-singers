05324f2a335528b2266d75f51a30423b2a8c36e3
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 08:37:49 -0700

    import singers component, add to routes array

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,12 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SingersComponent } from './singers/singers.component';
 
-const appRoutes: Routes = [ ];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: SingersComponent
+  }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
