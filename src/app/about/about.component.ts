ee51e5c95a5bdd9dbeb1a1b5658fc75acdc0f9a7
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 11:06:31 -0700

    import Singer model

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { Singer } from '../singer.model';
 
 @Component({
   selector: 'app-about',
