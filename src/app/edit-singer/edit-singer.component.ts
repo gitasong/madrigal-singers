5e8ed9dc3db387e4f3681d9f68d4b4b89a8bddb6
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 14:37:08 -0700

    import SingerService; add to providers array

--- src/app/edit-singer/edit-singer.component.ts
+++ src/app/edit-singer/edit-singer.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Singer } from '../singer.model';
+import { SingerService } from '../singer.service';
 
 @Component({
   selector: 'app-edit-singer',
   templateUrl: './edit-singer.component.html',
-  styleUrls: ['./edit-singer.component.css']
+  styleUrls: ['./edit-singer.component.css'],
+  providers: [SingerService]
 })
 
 export class EditSingerComponent implements OnInit {
