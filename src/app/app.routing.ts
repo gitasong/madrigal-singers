2b887732e6284e8343182b3d704ac26582340d30
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 09:21:49 -0700

    add about component to router & Routes array

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SingersComponent } from './singers/singers.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: SingersComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
