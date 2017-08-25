d65796f6fcb904bd8b536b53985b846efebbac9c
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 10:39:37 -0700

    add singers array to singers component

--- src/app/singers/singers.component.ts
+++ src/app/singers/singers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Singer } from '../singer.model';
-import { SINGERS } from '../singer-data';
+// import { SINGERS } from './singer-data';
+
 
 @Component({
   selector: 'app-singers',
@@ -9,6 +10,16 @@ import { SINGERS } from '../singer-data';
   styleUrls: ['./singers.component.css']
 })
 export class SingersComponent implements OnInit {
+
+  singers: Singer[] = [
+    new Singer("Lennie Cottrell", "Tenor 2", "conductor, co-director", 0, 1),
+    new Singer("Nicole Freed", "Soprano 1", "co-director", 1, 2),
+    new Singer("Nathan Daniel", "Tenor 1", "treasurer, section leader", 2, 3),
+    new Singer("Alex Ibiza", "Soprano 2", "secretary, section leader", 3, 4),
+    new Singer("Omeran Abatu", "Baritone/Bass 1", "webmaster, section leader", 4, 5),
+    new Singer("Cindy Serrano", "Soprano 2", "singer", 5, 6),
+    new Singer("Kimiko Shuri", "Soprano 1", "singer", 6, 7)
+  ];
 
   constructor() { }
 
