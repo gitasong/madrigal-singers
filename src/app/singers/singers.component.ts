95456e80c03cb1b976a1b61fe5c165068a384757
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 09:54:39 -0700

    import SINGERS data array

--- src/app/singers/singers.component.ts
+++ src/app/singers/singers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Singer } from '../singer.model';
+import { SINGERS } from '../singer-data';
 
 @Component({
   selector: 'app-singers',
