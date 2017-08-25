8c8c2901f9df42823b79b526978546ac2192ceda
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 14:29:42 -0700

    import Input, Singer model; create @Input for selectedSinger

--- src/app/edit-singer/edit-singer.component.ts
+++ src/app/edit-singer/edit-singer.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Singer } from '../singer.model';
 
 @Component({
   selector: 'app-edit-singer',
   templateUrl: './edit-singer.component.html',
   styleUrls: ['./edit-singer.component.css']
 })
+
 export class EditSingerComponent implements OnInit {
+  @Input() selectedSinger;
 
   constructor() { }
 
