87667d6c0f56a91103e0ff00b0804346aad38947
Nicole Freed <nicole.freed@gmail.com>
2017-08-25 11:55:19 -0700

    import singer service; register as provider

--- src/app/singer-detail/singer-detail.component.ts
+++ src/app/singer-detail/singer-detail.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Singer } from '../singer.model';
+import { SingerService } from '../singer.service';
 
 
 @Component({
   selector: 'app-singer-detail',
   templateUrl: './singer-detail.component.html',
-  styleUrls: ['./singer-detail.component.css']
+  styleUrls: ['./singer-detail.component.css'],
+  providers: [SingerService]
 })
 
 export class SingerDetailComponent implements OnInit {
